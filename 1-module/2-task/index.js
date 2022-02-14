295ad9cb01bf0b5d8cf3af15aeb8a2866206eed2
Evgenii <gusev_en@cps.tver.ru>
2022-02-14 12:58:14 +0300

    Done

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,10 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name === null || name.length < 4 || name.indexOf(' ') >=0){
+    return false
+  } else(name)
+  return true
 }
 
 function sayHello() {
