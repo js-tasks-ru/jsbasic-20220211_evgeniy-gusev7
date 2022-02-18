686310d69d4d6cfef5effd219aca9e3c63ee86f3
Evgenii <gusev_en@cps.tver.ru>
2022-02-18 16:25:54 +0300

    Done module-2-3

--- 2-module/3-task/task.test.js
+++ 2-module/3-task/task.test.js
@@ -4,18 +4,18 @@ describe('2-module-3-task', () => {
     expect(calculator.sum()).toBe(8);
   });
 
-  // it('должно умножать числа', () => {
-  //   calculator.read(3, 5);
-  //   expect(calculator.mul()).toBe(15);
-  // });
+  it('должно умножать числа', () => {
+    calculator.read(3, 5);
+    expect(calculator.mul()).toBe(15);
+  });
 
-  // it('должно корректно складывать числа с 0 первым аргументом', () => {
-  //   calculator.read(0, 1);
-  //   expect(calculator.sum()).toBe(1);
-  // });
+  it('должно корректно складывать числа с 0 первым аргументом', () => {
+    calculator.read(0, 1);
+    expect(calculator.sum()).toBe(1);
+  });
 
-  // it('должно корректно складывать числа с 0 вторым аргументом', () => {
-  //   calculator.read(1, 0);
-  //   expect(calculator.sum()).toBe(1);
-  // });
+  it('должно корректно складывать числа с 0 вторым аргументом', () => {
+    calculator.read(1, 0);
+    expect(calculator.sum()).toBe(1);
+  });
 });
