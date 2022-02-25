5ba9d068662ed0faa91bc200708b4117e2409bd0
Evgenii <gusev_en@cps.tver.ru>
2022-02-25 11:36:01 +0300

    Done 3-5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {
+    min: null,
+    max: null
+}
+  let res = str.split(" ")
+  let res2 = res.map((elem) => Number(elem))
+  let res3 = res2.filter(function(elem){ return !Number.isNaN(elem)})
+
+ 
+  result.min = Math.min.apply (null, res3)
+  result.max = Math.max.apply (null, res3)
+   return result
 }
