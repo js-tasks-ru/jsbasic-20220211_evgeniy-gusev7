906a4f2740a8d7eb80e73258dcb04bb7b13bf566
Evgenii <gusev_en@cps.tver.ru>
2022-02-28 15:48:21 +0300

    Done 4-1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,5 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let param = document.createElement('ul')
+   friends.forEach((el) => param2.innerHTML += ` <li> ${el.firstName} ${el.lastName} </li>`)
+   return param
 }
